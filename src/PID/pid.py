12ed579fc12421d5354aa9b9f726db28fe1ddbc0
Anthony Caccia <acaccia@ulb.ac.be>
2015-12-01 15:21:30 +0100

    PID class: simple PID algorithm done

--- src/PID/pid.py
+++ src/PID/pid.py
@@ -1,12 +1,22 @@
-
+from time import time
 
 class PID():
-    def __init__(self, defaultPoint, kp=2., ki=0., kd=1.):
+    def __init__(self, defaultPoint, kp=0., ki=0., kd=0.):
         self.defaultPoint = defaultPoint
         self.kp = kp
         self.ki = ki
         self.kd = kd
         self.lastErr = 0.
         self.integral = 0.
+        self.lastTime = time()
 
-    
+    def compute(self, measured):
+        now = time()
+        deltaTime = now - lastTime
+        error = defaultPoint - measured
+        self.integral += error * deltaTime
+        res = self.kp * error + self.ki * self.integral +
+                self.kd * (error - self.lastErr) / deltaTime
+        self.lastErr = error
+        self.lastTime = now
+        return res
