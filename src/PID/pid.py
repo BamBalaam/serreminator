dbc3925e853ca162e57c170a1a30bab58d49be60
Nikita Marchant <nikita.marchant@gmail.com>
2015-12-16 19:20:38 +0100

    Modify PID to set values between a min and a max.

--- src/PID/pid.py
+++ src/PID/pid.py
@@ -2,14 +2,17 @@ from time import time
 
 
 class PID():
-    def __init__(self, defaultPoint, kp=0., ki=0., kd=0.):
+    def __init__(self, defaultPoint, kp=0., ki=0., kd=0., min=float("-inf"), max=float("inf")):
         self.defaultPoint = defaultPoint
         self.kp = kp
         self.ki = ki
         self.kd = kd
+        self.min = min
+        self.max = max
         self.lastMeasured = 0.
         self.integral = 0.
         self.lastTime = time()
+        self.previous_state = 0
 
     def compute(self, measured):
         now = time()
@@ -18,6 +21,11 @@ class PID():
         self.integral += error * deltaTime
         deltaMeasured = measured - self.lastMeasured
         res = self.kp * error + self.ki * self.integral - self.kd * deltaMeasured / deltaTime
+
+        res = self.previous_state + res
+        res = min(max(self.min, res), self.max)
+        self.previous_state = res
+
         self.lastMeasured = measured
         self.lastTime = now
         return res
