1710a299f1da5cce817a4868693c076b6db00363
Nikita Marchant <nikita.marchant@gmail.com>
2015-12-16 13:46:00 +0100

    Update arduino sketch

--- src/arduino/src/sketch.ino
+++ src/arduino/src/sketch.ino
@@ -19,15 +19,15 @@ Sensor sensors[] = {
 Rgb rgbs[] = {
 };
 
-DHT dht = DHT(2, DHT11);
+DHT dht = DHT(7, DHT11);
 DHTSensor DHTSensors[] = {
     DHTSensor("temp", dht, TEMPERATURE, 7),
     DHTSensor("humid", dht, HUMIDITY, 7)
 };
 
-/*ServoAnim servosAnim[] = {
-    ServoAnim("volets", 6)
-};*/
+ServoAnim servosAnim[] = {
+    // ServoAnim("volets", 6)
+};
 
 
 HAL_CREATE(hal, sensors, triggers, switchs, animations, rgbs, DHTSensors, servosAnim);
