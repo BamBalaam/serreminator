685911c24247341c81b73ca71c62afcce32c6780
Anthony Caccia <acaccia@ulb.ac.be>
2016-02-10 16:03:34 +0100

    [Control] MultiPID class added

--- src/Control/pid.py
+++ src/Control/pid.py
@@ -1,6 +1,22 @@
 from time import time
 
 
+class MultiPID:
+    def __init__(self, pidA, pidB):
+        self.pidA = pidA
+        self.pidB = pidB
+        self.current = self.pidA
+
+    def toggle(self):
+        self.current = self.pidA if self.current is self.pidA else self.pidB
+
+    def run(self, measured):
+        state = self.current.compute(measured)
+        if state == self.current.min:
+            self.toggle()
+            state = self.current.compute(measured)
+        return state
+
 class PID():
     def __init__(self, defaultPoint, kp=0., ki=0., kd=0., min=float("-inf"), max=float("inf")):
         self.defaultPoint = defaultPoint
