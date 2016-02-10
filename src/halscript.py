ca7505bad9e9ebe83ef05d57a06ff607b4909968
Nikita Marchant <nikita.marchant@gmail.com>
2016-02-10 17:06:35 +0100

    Use crossbar

--- src/halscript.py
+++ src/halscript.py
@@ -6,17 +6,14 @@ from PID.pid import PID
 import asyncio
 import converters
 import os
-
-import matplotlib.pyplot as plt
+from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
 
 
 logging.basicConfig(
-    stream=stdout,
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)7s: %(message)s"
-)
-
+    stream=stdout, level=logging.DEBUG,
+    format="%(asctime)s %(levelname)7s: %(message)s %(traceback)s")
 logger = logging.getLogger(__name__)
+
 
 LUXMETER = {
     "Ra": 10900,
@@ -32,103 +29,65 @@ THERMISTANCE = {
 }
 
 
-def pid(HAL):
-    pid = PID(800, 0.15, min=0, max=255)
+class MyComponent(ApplicationSession):
+    def __init__(self, *args, **kwargs):
+        self.hal = HAL("/tmp/hal")
+        self.hal.animations.led.upload([0])
+        self.hal.animations.led.looping = True
+        self.hal.animations.led.playing = True
 
-    hal.animations.led.upload([0])
-    hal.animations.led.looping = True
-    hal.animations.led.playing = True
+        self.pid = PID(800, 0.15, min=0, max=255)
+        asyncio.async(self.adjust())
 
-    history = []
-    i = 0
-    while True:
-        mean = 0
-        for _ in range(5):
-            analogRead = HAL.sensors.lux.value
-            resistance = converters.tension2resistance(analogRead, 10000)
-            lux = converters.resistance2lux(resistance, **LUXMETER)
-            mean += lux
-            yield from asyncio.sleep(0.1)
-        lux = round(mean / 5, 2)
+        super().__init__(*args, **kwargs)
 
-        res = int(pid.compute(lux))
-        logger.info("Obs=%s, PID asks %s", lux, res)
+    async def onJoin(self, details):
+        while True:
+            self.send_data()
+            await asyncio.sleep(0.5)
 
-        hal.animations.led.upload([res])
-
-        history.append((800, lux))
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax2 = fig.add_subplot(111)
-        ax.plot(history)
-        ax2.plot(history)
-        plt.show()
-        plt.savefig('myfig')
-        os.rename('myfig.png', 'my.png')
-        yield from asyncio.sleep(0.5)
-        i += 1
-
-
-def dump(HAL):
-
-
-    while True:
-        dht = HAL.DHTsensors.temp.value
-        analogRead = HAL.sensors.temp.value
-        resistance = converters.tension2resistance(analogRead, 10000)
-        temp = converters.resistance2celcius(resistance, **THERMISTANCE)
-
-        analogRead = HAL.sensors.lux.value
+    def luxmeter(self):
+        analogRead = self.hal.sensors.lux.value
         resistance = converters.tension2resistance(analogRead, 10000)
         lux = converters.resistance2lux(resistance, **LUXMETER)
+        return lux
 
-        logger.debug("Temp=%s DHT=%s lux=%s", temp, dht, lux)
-        yield from asyncio.sleep(1)
+    def dht(self):
+        return self.hal.DHTsensors.temp.value
 
+    def thermistor(self):
+        analogRead = self.hal.sensors.temp.value
+        resistance = converters.tension2resistance(analogRead, 10000)
+        temp = converters.resistance2celcius(resistance, **THERMISTANCE)
+        return temp
 
-def demo(HAL):
-    logger.setLevel(logging.INFO)
-    print("T° %s" % hal.DHTsensors.temp.value)
-    print("Humidity %s" % hal.DHTsensors.humid.value)
+    def send_data(self):
+        self.publish('sensor.lux', self.luxmeter())
+        self.publish('sensor.temp', self.thermistor())
+        self.publish('sensor.dht.temp', self.dht())
 
-    analogRead = HAL.sensors.temp.value
-    resistance = converters.tension2resistance(analogRead, 10000)
-    temp = converters.resistance2celcius(resistance, **THERMISTANCE)
+    async def adjust(self):
+        while True:
+            mean = 0
+            for _ in range(5):
+                mean += self.luxmeter()
+                await asyncio.sleep(0.1)
+            lux = round(mean / 5, 2)
 
-    print("Analog T° %s" % temp)
+            res = int(self.pid.compute(lux))
+            self.publish('pid.light', res)
 
-    analogRead = HAL.sensors.lux.value
-    resistance = converters.tension2resistance(analogRead, 10000)
-    lux = converters.resistance2lux(resistance, **LUXMETER)
-
-    print("Lux %s" % lux)
-
-    hal.animations.led.playing = False
-
-    hal.animations.ventilo.looping = True
-    hal.animations.ventilo.playing = True
-    hal.animations.ventilo.upload([126])
-    yield from asyncio.sleep(3)
-    hal.animations.ventilo.upload([255])
-    yield from asyncio.sleep(3)
-    hal.animations.ventilo.upload([0])
-    yield from asyncio.sleep(3)
-
-    hal.animations.led.looping = True
-    hal.animations.led.playing = True
-    hal.animations.led.upload([126])
-    yield from asyncio.sleep(3)
-    hal.animations.led.upload([255])
-    yield from asyncio.sleep(3)
-
-    hal.animations.led.upload([0])
+            self.hal.animations.led.upload([res])
+            await asyncio.sleep(0.5)
 
 
-loop = asyncio.get_event_loop()
+if __name__ == '__main__':
+    runner = ApplicationRunner(url=u"ws://localhost:8080/ws", realm=u"realm1", debug=True)
+    runner.run(MyComponent)
 
-hal = HAL("/tmp/hal")
-loop.create_task(pid(hal))
-# loop.create_task(dump(hal))
-# loop.create_task(demo(hal))
+# loop = asyncio.get_event_loop()
 
-hal.run(loop=loop)
+# hal = HAL("/tmp/hal")
+# loop.create_task(pid(hal))
+
+# hal.run(loop=loop)
