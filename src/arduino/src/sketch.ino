6246e421c93093afac31ed48bf5e15085040a148
Romain Fontaine <romain-fontaine@outlook.com>
2016-02-22 14:07:23 +0100

    goto master

--- src/arduino/src/sketch.ino
+++ src/arduino/src/sketch.ino
@@ -20,18 +20,18 @@ Sensor sensors[] = {
 Rgb rgbs[] = {
 };
 
-DHT dht = DHT(7, DHT11);
-DHTSensor DHTSensors[] = {
-    DHTSensor("temp", dht, TEMPERATURE, 7),
-    DHTSensor("humid", dht, HUMIDITY, 7)
-};
+//DHT dht = DHT(7, DHT11);
+//DHTSensor DHTSensors[] = {
+//    DHTSensor("temp", dht, TEMPERATURE, 7),
+//    DHTSensor("humid", dht, HUMIDITY, 7)
+//};
 
 // ServoAnim servosAnim[] = {
 //     // ServoAnim("volets", 6)
 // };
 
 
-HAL_CREATE(hal, sensors, triggers, switchs, animations, rgbs, DHTSensors);
+HAL_CREATE(hal, sensors, triggers, switchs, animations, rgbs);
 
 void setup(){
     hal.setup();
