ed3b6291512028a0bee06bb55b79f7d0b4caef85
Anthony Caccia <acaccia@ulb.ac.be>
2016-02-24 10:54:03 +0100

    Late commit: genitic pid: proof of concept

---
+++ src/Control/GeneticPID.py
@@ -0,0 +1,108 @@
+import random
+
+class Chromosome:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+class Genetic:
+    def __init__(self, pop_size=100, mut_prob=0.1, cross_rate=0.9, max_gain=1.):
+        self.pop_size = pop_size
+        self.mut_prob = mut_prob
+        self.cross_rate = cross_rate
+        self.max_gain = max_gain
+
+    def mutation(self, c):
+        choice = random.random()
+        if choice < self.mut_prob / 3:
+            c.kp += random.random() * self.max_gain
+        elif choice < 2 * self.mut_prob / 3:
+            c.ki += random.random() * self.max_gain
+        elif choice < self.mut_prob:
+            c.kd += random.random() * self.max_gain
+        return c
+
+    def crossover(self, c1, c2):
+        if random.random() > self.cross_rate:
+            return c1
+        return Chromosome(*{1: (c2.kp, c1.ki, c1.kd),
+                2: (c2.kp, c2.ki, c1.kd),
+                3: (c1.kp, c1.ki, c2.kd),
+                4: (c1.kp, c2.ki, c1.kd),
+                5: (c2.kp, c2.ki, c2.kd)
+                }[random.randint(1,5)])
+
+    def fitness(self, dx):
+        print(dx)
+        return 1 / sum(d*d for d in dx)**.5
+
+    def selection(self, fs):
+        norm = (lambda s: [f / s for f in fs])(sum(fs))
+
+        cumul = [norm[0]]
+        for i in norm[1:]:
+            cumul.append(cumul[-1]+i)
+
+        newPop, parents = [], 0
+        while parents < 2:
+            r, i = random.random(), 0
+            while i < self.pop_size:
+                if r >= cumul[i] and r < cumul[i+1]:
+                    newPop.append(i)
+                    parents += 1
+                    i = self.pop_size
+                i += 1
+        return newPop
+
+class geneticPID:
+    def __init__(self, genetic, defaultPoint, timesteps=100, max_runs=300):
+        self.genetic = genetic
+        self.defaultPoint = defaultPoint
+        self.timesteps = timesteps
+        self.max_runs = max_runs
+        self.population = [Chromosome(random.random(), random.random(), random.random()) for _ in range(self.genetic.pop_size)]
+
+    def run(self, index):
+        """It's just a simple PID that virtually runs a lot of times"""
+        dists = []
+        current_pos = 0
+        current_dist = 0
+        last_dist = 0
+        integral = 0
+
+        for t in range(self.timesteps):
+            current_dist = self.defaultPoint - current_pos
+            
+            integral += current_dist
+            current_pos += self.population[index].kp * current_dist + \
+                self.population[index].kd * (current_dist - last_dist) + \
+                self.population[index].ki * integral
+            dists.append(current_dist)
+            last_dist = current_dist
+
+        return self.genetic.fitness(dists)
+
+    def next_generation(self):
+        next_gen = []
+        fitnesses = []
+        for i in range(self.genetic.pop_size):
+            fitnesses.append(self.run(i))
+
+        for i in range(self.genetic.pop_size):
+            p1, p2 = self.genetic.selection(fitnesses)
+            next_gen.append(self.genetic.mutation(self.genetic.crossover(self.population[p1], self.population[p2])))
+
+        self.fitnesses = fitnesses
+        self.population = next_gen
+
+    def find_parameters(self):
+        for _ in range(self.max_runs):
+            self.next_generation()
+        max_index = self.fitnesses.index(max(self.fitness))
+        return self.population[max_index].kp, self.population[max_index].ki, self.population[max_index].kd
+
+if __name__ == '__main__':
+    g = Genetic()
+    gpid = geneticPID(g, 800)
+    print(gpid.find_parameters())
