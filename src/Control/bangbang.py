df3dd1bf725b25aebd954df34f8eeeac5c63c2ac
Nikita Marchant <nikita.marchant@gmail.com>
2016-02-26 00:24:39 +0100

    Ah bravo Nils^Wacaccia

--- src/Control/bangbang.py
+++ src/Control/bangbang.py
@@ -2,7 +2,7 @@ class BangBang:
     def __init__(self, setpoint, deviation=0., positive_feedback=True):
         self.setpoint = setpoint
         self.deviation = deviation
-        self.run = _run_pos if positive_feedback else _run_neg
+        self.run = self._run_pos if positive_feedback else self._run_neg
 
     def _run_pos(self, state):
         return state < self.setpoint - self.deviation
