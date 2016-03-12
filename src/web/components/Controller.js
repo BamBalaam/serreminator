1b53a742cebc29c8a490d2b706aec6fea3a6c547
Nikita Marchant <nikita.marchant@gmail.com>
2016-03-12 18:32:02 +0100

    Much wow, such improvement, very colors

--- src/web/components/Controller.js
+++ src/web/components/Controller.js
@@ -2,18 +2,31 @@ const React = require("react");
 
 
 const Controller = React.createClass({
+    getInitialState: function(){
+        return {pressed: false};
+    },
     mouseDown: function(){
+        this.setState({pressed: true});
         this.props.session.call(this.props.topic, [true]);
     },
     mouseUp: function(){
+        this.setState({pressed: false});
         this.props.session.call(this.props.topic, [false]);
     },
     componentDidMount: function(){
 
     },
     render: function() {
+        var type = "btn-success";
+        if(!this.props.is_manual){
+            type = "";
+        }
+        else if(this.state.pressed){
+            type = "btn-warning";
+        }
+
         return <button
-            className="btn btn-success btn-block"
+            className={"btn btn-block " + type}
             disabled={this.props.is_manual ? "" : "disabled"}
             onMouseUp={this.mouseUp}
             onMouseDown={this.mouseDown}>
