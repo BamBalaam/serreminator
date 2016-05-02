92bdee98fb273d98f675c22c6e1083d7f09ba714
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-02 17:47:08 +0200

    Simpler simplescript

--- src/arduino/src/sketch.ino
+++ src/arduino/src/sketch.ino
@@ -4,35 +4,20 @@ Trigger triggers[] = {
 };
 
 Switch switchs[] = {
-    Switch("led_blue", 13),
-    Switch("led_green", 8),
-    Switch("led_red", 12),
-    Switch("heater_house", 4),
-    Switch("heater_box", 2),
 };
 
 Animation animations[] = {
-    Animation("fan_box", 11),
-    Animation("fan_house", 9),
-    Animation("strip_white", 5),
-    Animation("strip_blue", 3),
-    Animation("strip_red", 6)
+    Animation("strip_white", 3),
 };
 
 Sensor sensors[] = {
     Sensor("lux", 0),
-    Sensor("temp_box", 5),
-    Sensor("temp_house", 1),
-    Sensor("humudity_ground", 3)
 };
 
 Rgb rgbs[] = {};
 
-DHT dht = DHT(7, DHT11);
 
 DHTSensor DHTSensors[] = {
-    DHTSensor("temp", dht, TEMPERATURE, 7),
-    DHTSensor("humid", dht, HUMIDITY, 7)
 };
 
 
